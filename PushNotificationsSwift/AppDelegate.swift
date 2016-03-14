29aa4360245c4185bf03ee28622aeaa9a2138dbc
Nathan Hazout <nathanh@il.ibm.com>
2016-03-14 11:07:27 +0200

    Update AppDelegate

--- PushNotificationsSwift/AppDelegate.swift
+++ PushNotificationsSwift/AppDelegate.swift
@@ -32,33 +32,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
         // Initialize MFPPush
         MFPPush.sharedInstance().initialize()
-        RememberMeChallengeHandler()
+        
+        // Initialize challenge handler
+        _ = RememberMeChallengeHandler()
+        
+        // Check if application was opened from a notification
+        if let userInfo = launchOptions?[UIApplicationLaunchOptionsRemoteNotificationKey] as? [NSObject: AnyObject] {
+            //handle your notification
+            print("Recieved Notification in didFinishLaunchingWithOptions \(userInfo)")
+            
+        }
+        
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        print("Recieved Notification in didReceiveRemoteNotification \(userInfo)")
+        
+        // display the alert body
+        if let notification = userInfo["aps"] as? NSDictionary,
+            let alert = notification["alert"] as? NSDictionary,
+            let body = alert["body"] as? String {
+                showAlert(body)
+        }
+        
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        print("didRegisterForRemoteNotificationsWithDeviceToken: Registered device successfully")
+        
+        // Registers device token with server.
+        MFPPush.sharedInstance().sendDeviceToken(deviceToken)
     }
 
     func showAlert(message: String) {
@@ -68,23 +74,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window!.rootViewController?.presentViewController(alertDialog, animated: true, completion: nil)
     }
 
-    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
-        print("didRegisterForRemoteNotificationsWithDeviceToken: Registered device successfully")
-
-        // Registers device token with server.
-        MFPPush.sharedInstance().sendDeviceToken(deviceToken)
-    }
-
-    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        print("Recieved Notification \(userInfo)")
-        
-        // display the alert body
-        if let notification = userInfo["aps"] as? NSDictionary,
-            let alert = notification["alert"] as? NSDictionary,
-            let body = alert["body"] as? String {
-                showAlert(body)
-        }
-
-    }
 
 }
